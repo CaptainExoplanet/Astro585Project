07b5651fd03b24b347b37cbba86d8250c140da1b
CaptainExoplanet <jmjacks66@gmail.com>
2015-10-08 23:11:34 -0400

    switching to normal distribution

--- first_attempt.jl
+++ first_attempt.jl
@@ -1,10 +1,14 @@
 using Distributions
 srand(123)
 
-function iterate(p_i,q,psi,N::Int=1000)
+function iterate(p_i,psi::Array{4},N::Int=1000)
   w = zeros(N);
   sum = 0.0;
-  theta = rand(q,N);
+  theta = zeros(N);
+  for i in 1:N
+    for j in 1:lenght(psi[:,1])
+      sum += psi[j,2]
+  #theta = rand(q,N);
   for i in 1:N
     w[i] = exp(logpdf(p_i,theta[i])-logpdf(q,theta[i]));
     sum += w[i];
@@ -25,7 +29,7 @@ function main_func(p::Array,q,big_i::Int=1000)
   end
 end
 
-# setup q
+# setup psi
 function build_psi(alpha::Array{Float64,1}=ones(10)./10,df::Array{Int8,1}=ones(Int8,10).*2,x::Array{Float64,1}=[1.,2.,3.,4.,5.,6.,7.,8.,9.,10.])
   q = TDist[]
   for i in 1:length(df)
