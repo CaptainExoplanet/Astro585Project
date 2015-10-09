f839a88c9b39ca53495cbe66d13208284e92090f
CaptainExoplanet <jmjacks66@gmail.com>
2015-10-09 00:59:22 -0400

    need to add target distribution input

--- first_attempt.jl
+++ first_attempt.jl
@@ -25,31 +25,32 @@ function iterate(p_i,psi::Array{4},N::Int=1000)
 end
 
 function main_func(p::Array,q,big_i::Int=1000)
-  i=1
+  i=2
+  psi = build_psi();
   while i<big_i
-    psi[i] = iterate(p[i],q,psi[i-1])
+    psi = iterate(p,psi)
     i++
   end
 end
 
 # setup psi
 function build_psi(alpha::Array{Float64,1}=ones(10)./10,df::Array{Int8,1}=ones(Int8,10).*2,x::Array{Float64,1}=[1.,2.,3.,4.,5.,6.,7.,8.,9.,10.])
-  q = Normal[]
+  q = Normal[];
   for i in 1:length(df)
-    q=vcat(q,Normal(x[i],df[i]))
+    q=vcat(q,Normal(x[i],df[i]));
   end
-  psi = [alpha df x q]
+  psi = [alpha df x q];
 end
 
-function update_comp(theta,w,psi) #add types
+function update_comp(theta,w,psi,dim::Int=1) #add types
   for i in 1:length(psi[:,1])
     for j in 1:length(theta)
       epsilon[i,j] = psi[i,1]*pdf(psi[i,4],theta[j])*length(theta)/theta[j]; #also could be wrong
       alpha_prime[i] += pdf(psi[i,4],theta[j])*epsilon[i,j]
-      #x_prime[i] += stuff
+      #should have an x_prime calculation here
     end
   end
-  return build_psi(alpha_prime,psi[:,2],x_prime)
+  return build_psi(alpha_prime,psi[:,2],psi[:,3])
 end
 
 
