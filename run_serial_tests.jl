a1e87eac985a55de39a0e1da0d9e9b80783cda4b
CaptainExoplanet <jmjacks66@gmail.com>
2015-12-10 14:07:53 -0500

    tests ready for submission

--- run_serial_tests.jl
+++ run_serial_tests.jl
@@ -1,7 +1,5 @@
 #This file tests and benchmarks the serial version of the algorthm at varying numbers of iterations and samples.
 
-#cd("$(homedir())/Astro585Project");
-
 include("testing_utilities.jl");
 
 #run once with small N to compile
@@ -25,9 +23,6 @@ println("Running test in 2 dimensions with 100 iterations and 1000 samples");
 println("Running test in 2 dimensions with 1000 iterations and 1000 samples");
 @time test_run_algorithm_2D(1000,1000,1.);
 
-println("Running test in 2 dimensions with 10000 iterations and 1000 samples");
-@time test_run_algorithm_2D(10000,1000,1.);
-
 
 ### Test algorithm at varying N in 3 dimentions ###
 
@@ -45,8 +40,5 @@ println("Running test in 3 dimensions with 100 iterations and 1000 samples");
 
 println("Running test in 3 dimensions with 1000 iterations and 1000 samples");
 @time test_run_algorithm_3D(1000,1000,1.);
-
-println("Running test in 3 dimensions with 10000 iterations and 1000 samples");
-@time test_run_algorithm_3D(10000,1000,1.);
 
 println("All tests have been passed!");
