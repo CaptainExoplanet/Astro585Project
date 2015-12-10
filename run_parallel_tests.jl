d72ba69497a53e3a8157b75050705e8a6283e4ba
CaptainExoplanet <jmjacks66@gmail.com>
2015-12-10 04:03:15 -0500

    commented out for parallel again

--- run_parallel_tests.jl
+++ run_parallel_tests.jl
@@ -12,20 +12,20 @@ test_run_algorithm_3D(10,100,2.);
 
 ### Test algorith at varying N in 2 dimensions ###
 
-println("Running test in 2 dimensions with 10 iterations and 100 samples");
-@time test_run_algorithm_2D(10,100,2.);
+#println("Running test in 2 dimensions with 10 iterations and 100 samples");
+#@time test_run_algorithm_2D(10,100,2.);
 
-println("Running test in 2 dimensions with 10 iterations and 1000 samples");
-@time test_run_algorithm_2D(10,1000,2.);
+#println("Running test in 2 dimensions with 10 iterations and 1000 samples");
+#@time test_run_algorithm_2D(10,1000,2.);
 
-println("Running test in 2 dimensions with 10 iterations and 10000 samples");
-@time test_run_algorithm_2D(10,10000,2.);
+#println("Running test in 2 dimensions with 10 iterations and 10000 samples");
+#@time test_run_algorithm_2D(10,10000,2.);
 
-println("Running test in 2 dimensions with 100 iterations and 1000 samples");
-@time test_run_algorithm_2D(100,1000,1.);
+#println("Running test in 2 dimensions with 100 iterations and 1000 samples");
+#@time test_run_algorithm_2D(100,1000,1.);
 
-println("Running test in 2 dimensions with 1000 iterations and 1000 samples");
-@time test_run_algorithm_2D(1000,1000,1.);
+#println("Running test in 2 dimensions with 1000 iterations and 1000 samples");
+#@time test_run_algorithm_2D(1000,1000,1.);
 
 println("Running test in 2 dimensions with 10000 iterations and 1000 samples");
 @time test_run_algorithm_2D(10,100000,1.);
